d2891f90a6559c168abb86ab575952195c286108
Young-hoon Hong <eric.hong81@nayuda.com>
2014-03-21 16:51:00 +0900

    add new files

--- config/template/plugins/block.I.php
+++ config/template/plugins/block.I.php
@@ -1,12 +1,12 @@
 <?php
 /*
  * Smarty plugin
- * -------------------------------------------------------------
+ * ----------------------------------------------------------------------
  * File:     block.I.php
  * Type:     block
  * Name:     I
- * Purpose:  refrence the code in the file of CONFIG_LANG_PATH
- * -------------------------------------------------------------
+ * Purpose:  supporting International /config/lang/[lang].xml
+ * -----------------------------------------------------------------------
  */
 function smarty_block_I($params, $content, &$smarty, &$repeat)
 {
